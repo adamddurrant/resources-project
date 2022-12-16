5035eb8a5295dcdb66a9820ca19ad2747085026f
Adam Durrant <adamdurrant92@gmail.com>
2022-12-16 23:53:43 +0000

    styling for dekstop and logic for shortlist

--- src/components/Header.js
+++ src/components/Header.js
@@ -35,8 +35,8 @@ export const Heading = () => {
           <div className='mx-auto max-w-3xl pt-10 pb-32 sm:pt-40 sm:pb-40'>
             <div>
               <div className='hidden sm:mb-8 sm:flex sm:justify-center'>
-                <div className='relative overflow-hidden rounded-full py-1.5 px-4 text-sm leading-6'>
-                  <span className='text-gray-600 uppercase'>
+                <div className='relative overflow-hidden rounded-full py-1.5 px-4 text-sm leading-6 ring-1 ring-gray-900/10 hover:ring-gray-900/20'>
+                  <span className='text-gray-600 uppercase rounded'>
                     The SEO Toolbox
                   </span>
                 </div>
@@ -62,9 +62,9 @@ export const Heading = () => {
                   </a>
                 </div>
               </div>
-              <div class='absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]'>
+              <div className='absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]'>
                 <svg
-                  class='relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]'
+                  className='relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]'
                   viewBox='0 0 1155 678'
                   fill='none'
                   xmlns='http://www.w3.org/2000/svg'
--- src/components/ResourceSection.js
+++ src/components/ResourceSection.js
@@ -4,16 +4,19 @@ import { Card } from './Card.js';
 
 export const ResourceSection = () => {
   return (
-    <div className='relative z-0 overflow-hidden mt-3 block px-16'>
-      {resourceData.map((data) => (
-        //Iterate through resourceData array with callback function and map it to props sent to every card
-        <Card
-          key={data.ID}
-          imgSource={data.Logo}
-          name={data.Name}
-          description={data.Description}
-        />
-      ))}
-    </div>
+    <>
+      <div className='relative z-0 overflow-hidden mt-3 block px-16'>
+        {resourceData.map((data) => (
+          //Iterate through resourceData array with callback function and map it to props sent to every card
+          <Card
+            key={data.ID}
+            imgSource={data.Logo}
+            name={data.Name}
+            description={data.Description}
+            top={data.Favourite}
+          />
+        ))}
+      </div>
+    </>
   );
 };
